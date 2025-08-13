315f97b01308680e6bfcdd7218a40966ee32689c
Callum Jackson <honest.pollens-5j@icloud.com>
2025-08-13 14:30:24 +1000

    Implement timer, replay screen, and changed font.
    - Replaced orbs remaining with timer as scoring system.
    - Implement replay screen to display final time and wait for restart.
    - Update font to "Press Start" font.

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,29 +1,41 @@
 extends Node
 
 var orbs_remaining
+var elapsed_time: float = 0.0
+var time = "00:00:00"
 
 func _ready() -> void:
 	var orbs = get_tree().get_nodes_in_group("orbs")
 	orbs_remaining = orbs.size()
 	for orb in orbs:
 		orb.hit.connect(_on_orb_hit)
-	$HUD/ScoreLabel.text = "Orbs Remaining: " + str(orbs_remaining)
 	$HUD/ScoreLabel.show()
 
-func _process(delta: float) -> void:
-	pass
 
+func _process(delta: float) -> void:
+	elapsed_time += delta
+	var minutes = int(elapsed_time / 60)
+	var seconds = int(elapsed_time) % 60
+	var centiseconds = int((elapsed_time - int(elapsed_time)) * 100)
+	time = "%02d:%02d:%02d" % [minutes, seconds, centiseconds] 
+	$HUD/ScoreLabel.text = "Time: " + str(time)
+	
 func _on_orb_hit() -> void:
 	orbs_remaining -= 1
-	$HUD/ScoreLabel.text = "Orbs Remaining: " + str(orbs_remaining)
 	if orbs_remaining == 0:
 		$Portal.enable_portal()
 
 func _on_portal_hit() -> void:
 	$HUD/ScoreLabel.hide()
-	$HUD/WinMessage.text = "You Win!"
+	$HUD/WinMessage.text = "Time: " + str(time)
 	$HUD/WinMessage.show()
+	$HUD/ReplayMessage.show()
 	$Player/AnimationPlayer.play("disappear")
-	# $Player.queue_free()
-	await get_tree().create_timer(3).timeout
-	get_tree().change_scene_to_file("res://scenes/start.tscn")
+	# await get_tree().create_timer(5).timeout
+	# get_tree().change_scene_to_file("res://scenes/start.tscn")
+	
+
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_accept"):
+		if $HUD/ReplayMessage.is_visible_in_tree():
+			get_tree().change_scene_to_file("res://scenes/main.tscn")
--- scripts/ui.gd
+++ scripts/ui.gd
@@ -2,3 +2,4 @@ extends CanvasLayer
 
 func _ready() -> void:
 	$WinMessage.hide()
+	$ReplayMessage.hide()
