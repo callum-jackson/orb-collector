93d93f9fb93ad39a21c6c3a197ee0a3bf7a4b912
Callum Jackson <honest.pollens-5j@icloud.com>
2025-08-13 21:21:22 +1000

    Randomise location of orbs for each play through.

--- scripts/main.gd
+++ scripts/main.gd
@@ -3,11 +3,19 @@ extends Node
 var orbs_remaining
 var elapsed_time: float = 0.0
 var time = "00:00:00"
+var screen_size
+var margin = 50
+var min_pos
+var max_pos
 
 func _ready() -> void:
+	screen_size = get_viewport().get_visible_rect().size
+	min_pos = Vector2(margin, margin)
+	max_pos = screen_size - Vector2(margin, margin)
 	var orbs = get_tree().get_nodes_in_group("orbs")
 	orbs_remaining = orbs.size()
 	for orb in orbs:
+		random_position(orb)
 		orb.hit.connect(_on_orb_hit)
 	$HUD/ScoreLabel.show()
 
@@ -19,7 +27,7 @@ func _process(delta: float) -> void:
 	var centiseconds = int((elapsed_time - int(elapsed_time)) * 100)
 	time = "%02d:%02d:%02d" % [minutes, seconds, centiseconds] 
 	$HUD/ScoreLabel.text = "Time: " + str(time)
-	
+
 func _on_orb_hit() -> void:
 	orbs_remaining -= 1
 	if orbs_remaining == 0:
@@ -31,11 +39,15 @@ func _on_portal_hit() -> void:
 	$HUD/WinMessage.show()
 	$HUD/ReplayMessage.show()
 	$Player/AnimationPlayer.play("disappear")
-	# await get_tree().create_timer(5).timeout
-	# get_tree().change_scene_to_file("res://scenes/start.tscn")
-	
 
 func _unhandled_input(event):
 	if event.is_action_pressed("ui_accept"):
 		if $HUD/ReplayMessage.is_visible_in_tree():
 			get_tree().change_scene_to_file("res://scenes/main.tscn")
+
+func random_position(node):
+	var random_pos = Vector2(
+		randf_range(min_pos.x, max_pos.x),
+		randf_range(min_pos.y, max_pos.y)
+	)
+	node.position = random_pos
